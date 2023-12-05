d92f0777957db7d465da0fb80ba8a0f760d00ad8
clari62 <clariespertino62@gmail.com>
2023-12-05 14:09:21 -0300

    desarrollo mission 4

---
+++ public/js/glide.js
@@ -0,0 +1,15 @@
+//MIssions 3: Andrea Llavel
+new Glide('.glide', {
+    type: 'carousel',
+    startAt: 0,
+    perView: 3,
+    gap: 30,
+    breakpoint: {
+        991: {
+            perView: 2
+        },
+        768: {
+            perView: 1
+        }
+    }
+}).mount();
