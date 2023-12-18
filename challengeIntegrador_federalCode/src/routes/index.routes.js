93c1f46c2988cdb1937f3f8e810179e4ffee9a97
Andrea Llavel <andryllavel20@gmail.com>
2023-12-18 20:37:14 +0100

    agregando funcionalidad al crud

--- challengeIntegrador_federalCode/src/routes/index.routes.js
+++ challengeIntegrador_federalCode/src/routes/index.routes.js
@@ -3,6 +3,16 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/index.controller')
 //Queremos acceder a la ruta, y a un callback
+//Rutas Get
 router.get('/', controller.index)
+//Rutas Post
+router.post('/add-user', controller.addUser)
+router.post('/update-user/:userId', controller.updateUser)
+//crear una variable , para poder acceder a ella, desde el controlador
+
+//PUT
+router.put(`/update-user/:userId`, controller.updateUser)
+//DELETE
+router.delete(`/delete-user/:userId`, controller.deleteUser)
 
 module.exports = router
